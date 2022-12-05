3b3652983a8a2290ce6a50c93d3344cd208f0c69
latifat99 <118016581+latifat99@users.noreply.github.com>
2022-12-05 10:01:58 +0100

    Add files via upload

---
+++ Javascript 5/script.js
@@ -0,0 +1,113 @@
+const characters = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+  "0",
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "~",
+  "`",
+  "!",
+  "@",
+  "#",
+  "$",
+  "%",
+  "^",
+  "&",
+  "*",
+  "(",
+  ")",
+  "_",
+  "-",
+  "+",
+  "=",
+  "{",
+  "[",
+  "}",
+  "]",
+  ",",
+  "|",
+  ":",
+  ";",
+  "<",
+  ">",
+  ".",
+  "?",
+  "/",
+];
+let passwordLength = 12;
+let pass = "";
+let content1 = document.getElementById("content");
+let content2 = document.getElementById("content2");
+function generate() {
+  let randomindex = Math.floor(Math.random() * characters.length);
+  return characters[randomindex];
+}
+function password() {
+  let pass = "";
+  for (let i = 0; i < passwordLength; i++) {
+    pass += generate();
+  }
+  return pass;
+}
+
+function display() {
+  content1.textContent = password();
+  content2.textContent = password();
+}
